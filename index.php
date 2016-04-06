bf64e49bc254e1e0c28bfa6f4c7492bd1a36b9a6
yyljj <yyljj@outlook.com>
2016-04-06 13:35:03 +0800

    白忙活一下午

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <script src="lib.js"></script>
+    <script src="love.js"></script>
 </head>
 <body onload="refreshLove()">
 <h3 id="love"></h3>

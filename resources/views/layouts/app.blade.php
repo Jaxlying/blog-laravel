f68bebdc08d3e385562fdca7e1ceba5ae39b695a
jaxlying <jaxlying@outlook.com>
2016-08-26 20:53:04 +0800

    navder bs3 add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,46 +4,44 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
+    <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
     <title>blog</title>
 </head>
 <body>
-    <nav>
+    <nav class="navbar navbar-inverse">
         <div>
             <div>
 
-                <!-- Collapsed Hamburger -->
-                <button type="button">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
                 <!-- Branding Image -->
-                <a href="{{ url('/') }}">
-                    Laravel
+                <a class="navbar-brand" href="{{ url('/') }}">
+                   首页
                 </a>
             </div>
 
-            <div>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
-                <ul>
-                    <li><a href="{{ url('/home') }}">Home</a></li>
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/admin/article') }}">管理文章</a></li>
+                    <li><a href="{{ url('/admin/comment') }}">管理评论</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul>
+                <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
-                        <li>
-                            <a href="#">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
-                            <ul>
-                                <li><a href="{{ url('/logout') }}"><i></i>Logout</a></li>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
                     @endif
@@ -52,5 +50,8 @@
         </div>
     </nav>
     @yield('content')
+
+    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+    <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,16 +4,21 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
+    <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
 
     <title>blod-laravel</title>
 
 </head>
 <body>
-<nav>
+<nav  class="navbar navbar-inverse">
     <div>
         <div>
             <!-- Branding Image -->
-            <a href="{{ url('/') }}">
+            <a class="navbar-brand" href="{{ url('/') }}">
                 主页
             </a>
         </div>
@@ -22,5 +27,8 @@
 
 @yield('content')
 
+
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
 </body>
 </html>
